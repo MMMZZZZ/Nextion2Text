3bfa80440305d4cdee8a6dceea3d16b7810da3a6
MMMZZZZ <36891873+MMMZZZZ@users.noreply.github.com>
2021-01-12 11:43:35 +0100

    Fixed License Note

--- Nextion2Text.py
+++ Nextion2Text.py
@@ -1,5 +1,5 @@
 """
-TFTTool by Max Zuidberg
+Nextion2Text by Max Zuidberg
 
 This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
